e0f6cc7be079a6aee81a756fe55b3a080319a40a
기남후 <skagn4929@naver.com>
2023-10-18 18:55:31 +0900

    10~12

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import { useState } from "react";
 
 function App() {
   let [글제목, 글제목변경] = useState([
-    "남자 코트 추천",
-    "강남 우동 맛집",
-    "파이썬 독학",
+    "남자 코트 추천 ",
+    "강남 우동 맛집 ",
+    "파이썬 독학 ",
   ]);
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState("");
 
   return (
     <div className="App">
@@ -69,10 +71,16 @@ function App() {
       {글제목.map(function (a, i) {
         return (
           <div className="list" key={i}>
-            <h4>
+            <h4
+              onClick={() => {
+                setModal(true);
+                setTitle(i);
+              }}
+            >
               {글제목[i]}
               <span
-                onClick={() => {
+                onClick={(e) => {
+                  e.stopPropagation();
                   let copy = [...따봉];
                   copy[i] = copy[i] + 1;
                   따봉변경(copy);
@@ -80,24 +88,34 @@ function App() {
               >
                 👍🏻
               </span>{" "}
-              {따봉[i]}{" "}
+              {따봉[i]} <button>삭제</button>
             </h4>
             <p>2월 17일 발행</p>
           </div>
         );
       })}
 
-      {modal == true ? <Modal /> : null}
+      <input
+        onChange={(e) => {
+          입력값변경(e.target.value);
+          console.log(입력값);
+        }}
+      ></input>
+
+      <button>글추가</button>
+
+      {modal == true ? <Modal 글제목={글제목} title={title} /> : null}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button>글수정</button>
     </div>
   );
 }
